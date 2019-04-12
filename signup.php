8bc5f6e955fb4ac1bd7e2424980ad4f17cb60d7d
Kelvin <g-kelvin>
2019-04-12 13:00:12 +0300

    yes

--- signup.php
+++ signup.php
@@ -1,11 +1,21 @@
 <?php 
 
-	$conn=mysqli_connect('localhost','root','kelvingauki','kelvin');
+	$conn=mysqli_connect('localhost','root','kelvingauki','consultancy');
 	if (!$conn){
-		echo "erroe";
+		echo "error ";
 	}
 	else {
-		echo "you are good kelvin";
+		$email = $_POST['email'];
+		$password = $_POST['password'];
+		$telephone = $_POST['telephone'];
+
+		if(mysqli_query($conn, "INSERT INTO regitser (email, password, telephone ) VALUES ('$email', '$password', '$telephone')")){
+			echo "you have successfully registered";
+		}
+		else
+		{
+			echo "error in regitration";
+		}
 	}
 
 
